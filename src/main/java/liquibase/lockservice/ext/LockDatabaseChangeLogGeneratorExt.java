7fc78c5919fa1b6e72c5e931ece959b75b891a91
arko0220 <artem.kostiuchik@netcracker.com>
2022-05-06 16:49:11 +0300

    migrate to liquibase-core:3.8.9

--- src/main/java/liquibase/lockservice/ext/LockDatabaseChangeLogGeneratorExt.java
+++ src/main/java/liquibase/lockservice/ext/LockDatabaseChangeLogGeneratorExt.java
@@ -1,11 +1,16 @@
 package liquibase.lockservice.ext;
 
-import liquibase.Scope;
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import liquibase.database.Database;
 import liquibase.datatype.DataTypeFactory;
 import liquibase.exception.DatabaseException;
 import liquibase.executor.ExecutorService;
-import liquibase.logging.Logger;
 import liquibase.sql.Sql;
 import liquibase.sqlgenerator.SqlGeneratorChain;
 import liquibase.sqlgenerator.SqlGeneratorFactory;
@@ -14,19 +19,14 @@ import liquibase.statement.core.LockDatabaseChangeLogStatement;
 import liquibase.statement.core.RawSqlStatement;
 import liquibase.statement.core.UpdateStatement;
 
-
-import java.sql.Timestamp;
-import java.util.List;
-import java.util.Map;
-
 public class LockDatabaseChangeLogGeneratorExt extends LockDatabaseChangeLogGenerator {
-    private final static Logger LOG = Scope.getCurrentScope().getLog(LockDatabaseChangeLogGeneratorExt.class);
 
     public static final String LOCKED_BY_SEPARATOR = "@@";
+    private static final Logger log = LoggerFactory.getLogger(LockDatabaseChangeLogGeneratorExt.class);
 
     @Override
     public int getPriority() {
-        return super.getPriority()+1000;
+        return super.getPriority() + 1000;
     }
 
     @Override
@@ -35,39 +35,39 @@ public class LockDatabaseChangeLogGeneratorExt extends LockDatabaseChangeLogGene
     }
 
     @Override
-    public Sql[] generateSql(LockDatabaseChangeLogStatement statement, Database database, SqlGeneratorChain sqlGeneratorChain) {
+    public Sql[] generateSql(LockDatabaseChangeLogStatement statement, Database database,
+                             SqlGeneratorChain sqlGeneratorChain) {
         String lockedByValue = generateLockedBy(database);
-
         String liquibaseSchema = database.getLiquibaseSchemaName();
         String liquibaseCatalog = database.getLiquibaseCatalogName();
-
-        UpdateStatement updateStatement = new UpdateStatement(liquibaseCatalog, liquibaseSchema, database.getDatabaseChangeLogLockTableName());
+        UpdateStatement updateStatement = new UpdateStatement(liquibaseCatalog, liquibaseSchema,
+                database.getDatabaseChangeLogLockTableName());
         updateStatement.addNewColumnValue("LOCKED", true);
         updateStatement.addNewColumnValue("LOCKGRANTED", new Timestamp(new java.util.Date().getTime()));
         updateStatement.addNewColumnValue("LOCKEDBY", lockedByValue);
-        updateStatement.setWhereClause(database.escapeColumnName(liquibaseCatalog, liquibaseSchema, database.getDatabaseChangeLogTableName(), "ID") + " = 1 AND " + database.escapeColumnName(liquibaseCatalog, liquibaseSchema, database.getDatabaseChangeLogTableName(), "LOCKED") + " = " + DataTypeFactory.getInstance().fromDescription("boolean", database).objectToSql(false, database));
-
+        updateStatement.setWhereClause(database.escapeColumnName(liquibaseCatalog, liquibaseSchema,
+                database.getDatabaseChangeLogTableName(), "ID") + " = 1 AND "
+                + database.escapeColumnName(liquibaseCatalog, liquibaseSchema,
+                database.getDatabaseChangeLogTableName(), "LOCKED")
+                + " = " + DataTypeFactory.getInstance()
+                .fromDescription("boolean", database).objectToSql(false, database));
         return SqlGeneratorFactory.getInstance().generateSql(updateStatement, database);
     }
 
     private String generateLockedBy(Database database) {
         String dbPid = "0000";
         String dbPidStartTime = "";
-
         try {
-            List<Map<String, ?>> rs = Scope.getCurrentScope().getSingleton(ExecutorService.class).getExecutor("jdbc", database).queryForList(
-                    new RawSqlStatement("select * from pg_stat_activity where pid=pg_backend_pid()"));
+            List<Map<String, ?>> rs = ExecutorService.getInstance().getExecutor(database)
+                    .queryForList(new RawSqlStatement("select * from pg_stat_activity where pid=pg_backend_pid()"));
             if (rs.size() > 0) { // expected exactly one row
                 Map<String, ?> row = rs.get(0);
-                dbPid = Integer.toString ((Integer) row.get("PID"));
-                dbPidStartTime = ((Timestamp)(row.get("BACKEND_START"))).toString();
+                dbPid = Integer.toString((Integer) row.get("PID"));
+                dbPidStartTime = row.get("BACKEND_START").toString();
             }
         } catch (DatabaseException e) {
-            LOG.severe("Failed to read current Liquibase locking info", e);
+            log.error("Failed to read current Liquibase locking info", e);
         }
-
-        String lockedByValue = dbPid + LOCKED_BY_SEPARATOR + dbPidStartTime;
-        LOG.warning("Setting LOCKEDBY value to " + lockedByValue);
-        return lockedByValue;
+        return dbPid + LOCKED_BY_SEPARATOR + dbPidStartTime;
     }
 }
